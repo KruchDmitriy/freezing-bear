9d09f380eff30383b81e40aea3ca8781f4ebb8c1
Vlad Vinogradov <vladt9@yandex.ru>
2015-05-26 20:27:04 +0300

    Added Interpolation file

---
+++ Algorithm/Interpolation.hs
@@ -0,0 +1,10 @@
+import ExpressionParser.ExpressionParser
+
+type Point = (Double, Double)
+
+get_diff :: Double -> Function
+get_diff xi = create_func "(x - " ++ (show xi) ++ ")"
+
+interpolate_lagrange :: [Point] -> Function
+interpolate_lagrange [] = create_func "0"
+interpolate_lagrange points = 
