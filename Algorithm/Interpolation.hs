12cbf57a52f191ad96b1b77fa050cda74694dd5b
Vlad Vinogradov <vladt9@yandex.ru>
2015-05-27 00:12:44 +0300

    Implemented Lagrange polynom

--- Algorithm/Interpolation.hs
+++ Algorithm/Interpolation.hs
@@ -1,13 +1,42 @@
 import ExpressionParser.Operator.BinaryOperator
 import ExpressionParser.ExpressionParser
 
+import Data.List
+
 type Point = (Double, Double)
 
-get_diff :: Double -> Function
-get_diff xi = create_func $ "(x - " ++ (show xi) ++ ")"
+get_diff_nom :: Double -> Function
+get_diff_nom xi = create_func $ "(x - " ++ (show xi) ++ ")"
 
+get_diff_denom :: Double -> Double -> Double
+get_diff_denom xi x = xi - x
 
+transform_points :: [Double] -> Int -> Int -> [Double]
+transform_points [] _ _ = []
+transform_points (d:ds) i n
+            | (i == n) = transform_points ds (i + 1) n
+            | otherwise = d : transform_points ds (i + 1) n
 
---interpolate_lagrange :: [Point] -> Function
---interpolate_lagrange [] = create_func "0"
---interpolate_lagrange points = 
+tr_list_points :: [Double] -> Int -> Int -> [[Double]]
+tr_list_points [] _ _ = [[]]
+tr_list_points points i len
+        | (i /= len) = transform_points points 0 i :
+            (tr_list_points points (i + 1) len)
+        | otherwise = []
+
+product_diffs_nom :: [Double] -> Function
+product_diffs_nom points = foldl' (*) (create_func "1") (map get_diff_nom points)
+
+product_diffs_denom :: Double -> [Double] -> Double
+product_diffs_denom xi points = foldl' (*) 1.0 (map (\ ps -> get_diff_denom ps xi) points)
+
+interpolate_lagrange :: [Point] -> Function
+interpolate_lagrange [] = create_func "0"
+interpolate_lagrange points = sum s
+    where
+        noms = map product_diffs_nom tr_points
+        denoms = map (create_func . show) $ zipWith (\ x y -> product_diffs_denom y x) tr_points fst_s
+        tr_points = tr_list_points fst_s 0 (length fst_s)
+        fst_s = map fst points
+        snd_s = map (create_func . show . snd) points
+        s = zipWith3 (\ x y z -> x / y * z) noms denoms snd_s
